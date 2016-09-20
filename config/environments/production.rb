11840103ac9dabf4eddc7ee9f64d15d011376ca8
Songtham Tungkitkancharoen <songthamtung@gmail.com>
2016-09-19 20:04:54 -0700

    updated prod env config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'https://crawling-ninja.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: '587',
+  domain: "herokuapp.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+}
 end
