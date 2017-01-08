50fa21bbf8839147cd12131a867ee82456787787
Songtham Tungkitkancharoen <songthamtung@gmail.com>
2017-01-07 23:40:56 -0800

    updated contact form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,6 +3,10 @@ class Contact < MailForm::Base
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message,   :validate => true
   attribute :nickname,  :captcha  => true
+  attribute :first_name, :validate => true
+  attribute :last_name
+  attribute :company
+  attribute :title
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
