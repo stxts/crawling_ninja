d564b612f96dd209bd68b05c4002cd161814fc9b
Songtham Tungkitkancharoen <songthamtung@gmail.com>
2017-01-26 21:50:50 -0800

    fixed redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  match "*path" => redirect("https://www.crawling.ninja/%{path}"), :constraints => { :protocol => "http://" }
+  match "*path" => redirect("https://www.crawling.ninja/%{path}"), :constraints => { :subdomain => "" }
   match '/contacts', to: 'contacts#create', via: ['get']
   resources "contacts", only: [:new, :create]
   root 'home#index'
