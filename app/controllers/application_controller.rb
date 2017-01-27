caf6aa7d188e431c913b7ced7fe71fd17e9069e0
Songtham Tungkitkancharoen <songthamtung@gmail.com>
2017-01-26 21:58:40 -0800

    revert redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  def redirect_https        
-        redirect_to :protocol => "https://" unless request.ssl?
-        return true
-    end
-    before_filter :redirect_https
   protect_from_forgery with: :exception
 end
