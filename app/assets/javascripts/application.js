48ad09f2f5c42064f708c1e7912eaa0b7caf82b9
Songtham Tungkitkancharoen <songthamtung@gmail.com>
2017-02-24 19:52:33 -0800

    testing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,7 +38,6 @@ if (navigator.userAgent.match(/Mobi/)) {
         $('.device-fixed-width').css('transform', 'scale(' + ds + ',1)')
             .css('transform-origin', '0 0');
         $('.device-fixed-height').css({'left':$('body').scrollLeft()‌​+'px','width':window‌​.innerWidth+'px'});
-
         })
 }
 
